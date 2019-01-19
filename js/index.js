63e628f8f6e3a5a55d64db6a173c1557e87b0ce2
Rick Buter <buter.rick@gmail.com>
2019-01-19 22:10:36 +0100

    slider is working, circles are not

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
 var indexSliderHomePage = 0;
-var indexSliderHomePageArray = ['/images/girl1.jpg', '/images/girl2.jpg', '/images/girl3.jpg', '/images/girl4.jpg', '/images/girl5.jpg'];
-var indexSliderHomeCounter = 4000;
+var sliderHomePageArray = ['/images/girl1.jpg', '/images/girl2.jpg', '/images/girl3.jpg', '/images/girl4.jpg', '/images/girl5.jpg'];
+
+var circle1 = document.getElementsByClassName('circle1');
+var circle2 = document.getElementsByClassName('circle2');
+var circle3 = document.getElementsByClassName('circle3');
+var circle4 = document.getElementsByClassName('circle4');
+var circle5 = document.getElementsByClassName('circle5');
 
 function startHomePageSlider() {
-    document.getElementsByClassName('navbar_container').src = indexSliderHomePageArray;
-    if (indexSliderHomePage > indexSliderHomePageArray.length - 1) {
+    document.homeSlider.src = sliderHomePageArray[indexSliderHomePage];
+    if (indexSliderHomePage < sliderHomePageArray.length - 1) {
         indexSliderHomePage++;
     } else {
         indexSliderHomePage = 0;
+        console.log(indexSliderHomePage)
     }
-}
-window.onload=startHomePageSlider;
+    setTimeout("startHomePageSlider()", 5000);
+
+    // if (indexSliderHomePage = 0) {
+    //     circle1.style.backgroundColor = 'black';
+    // } else if (indexSliderHomePage = 1) {
+    //     circle2.style.backgroundColor = 'black';
+    // } else if (indexSliderHomePage = 2) {
+    //     circle3.style.backgroundColor = 'black';
+    // } else if (indexSliderHomePage = 3) {
+    //     circle4.style.backgroundColor = 'black';
+    // }   else {
+    //     circle5.style.backgroundColor = 'black';
+    //     }
+    }
+window.onload = startHomePageSlider;
